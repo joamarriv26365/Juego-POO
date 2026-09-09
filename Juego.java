4a621a766ad9a98321e7f36aee81d8e1e6fa2a11
Paula90233 <mon26024@uvg.edu.gt>
2026-09-09 11:36:04 -0600

    Update Juego.java

--- Juego.java
+++ Juego.java
@@ -11,16 +11,16 @@ public class Juego {
         this.numpregunta = 0;
         this.puntaje = 0;
         preguntas = new Pregunta[10];
-        preguntas[0] = new Pregunta(1, "imagenes/pregunta1.jpg", true, "Retroalimentación 1");
-        preguntas[1] = new Pregunta(2, "imagenes/pregunta2.jpg",false, "Retroalimentación 2");
-        preguntas[2] = new Pregunta(3, "imagenes/pregunta3.jpg",true,"Retroalimentación 3");
-        preguntas[3] = new Pregunta(4, "imagenes/pregunta4.jpg",true,"Retroalimentación 4");
-        preguntas[4] = new Pregunta(5, "imagenes/pregunta5.jpg",true,"Retroalimentación 5");
-        preguntas[5] = new Pregunta(6, "imagenes/pregunta6.jpg",true,"Retroalimentación 6");
-        preguntas[6] = new Pregunta(7, "imagenes/pregunta7.jpg",true,"Retroalimentación 7");
-        preguntas[7] = new Pregunta(8, "imagenes/pregunta8.jpg",true,"Retroalimentación 8");
-        preguntas[8] = new Pregunta(9, "imagenes/pregunta9.jpg",true,"Retroalimentación 9");
-        preguntas[9] = new Pregunta(10, "imagenes/pregunta10.jpg",true,"RetroalimentacióN 10");
+        preguntas[0] = new Pregunta(1, "imagenes1.jpg", true, "Retroalimentación 1");
+        preguntas[1] = new Pregunta(2, "imagenes2.jpg",false, "Retroalimentación 2");
+        preguntas[2] = new Pregunta(3, "imagenes3.jpg",true,"Retroalimentación 3");
+        preguntas[3] = new Pregunta(4, "imagenes4.jpg",true,"Retroalimentación 4");
+        preguntas[4] = new Pregunta(5, "imagenes5.jpg",true,"Retroalimentación 5");
+        preguntas[5] = new Pregunta(6, "imagenes6.jpg",true,"Retroalimentación 6");
+        preguntas[6] = new Pregunta(7, "imagenes7.jpg",true,"Retroalimentación 7");
+        preguntas[7] = new Pregunta(8, "imagenes8.jpg",true,"Retroalimentación 8");
+        preguntas[8] = new Pregunta(9, "imagenes9.jpg",true,"Retroalimentación 9");
+        preguntas[9] = new Pregunta(10, "imagenes10.jpg",true,"RetroalimentacióN 10");
     }
 
     public Pregunta obtenerPreguntaActual() {
