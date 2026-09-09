aa06266e97afdfb76eacf763a517be5510519526
Paula90233 <mon26024@uvg.edu.gt>
2026-09-09 11:38:37 -0600

    Refactor constructor and methods in Juego class

--- Juego.java
+++ Juego.java
@@ -14,7 +14,7 @@ public class Juego {
         preguntas[0] = new Pregunta(1, "imagenes1.jpg", true, "Retroalimentación1");
         preguntas[1] = new Pregunta(2, "imagenes2.jpg",false, "Retroalimentación 2");
         preguntas[2] = new Pregunta(3, "imagenes3.jpg",true,"Retroalimentación 3");
-        preguntas[3] = new Pregunta(4, "imagenes4.jpg",true,"Retroalimentación 4");
+        preguntas[3] = new Pregunta(4, "imagenes4.jpg",true,"Retroalimentación4");
         preguntas[4] = new Pregunta(5, "imagenes5.jpg",true,"Retroalimentación 5");
         preguntas[5] = new Pregunta(6, "imagenes6.jpg",true,"Retroalimentación 6");
         preguntas[6] = new Pregunta(7, "imagenes7.jpg",true,"Retroalimentación 7");
